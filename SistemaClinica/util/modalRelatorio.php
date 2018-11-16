edbf03a8008024c8b5b3987da7c81c5b3f58e3b3
Denilson Soares <34691643+DreshGraphics@users.noreply.github.com>
2018-11-16 19:31:08 -0200

    att 2
    Co-Authored-By: felipesousa12 <felipesousa12@users.noreply.github.com>

--- SistemaClinica/util/modalRelatorio.php
+++ SistemaClinica/util/modalRelatorio.php
@@ -11,7 +11,7 @@
                           <form action="../Relatorio/Agenda/RelatorioAgendaProfissional.php" method="POST" target="_blank">
                           <div class="modal-body">
                             <div class="conteudo">              
-              					     	<select class="custom-select" style="text-transform: uppercase;" name="medico">
+              					     	<select class="custom-select" style="text-transform: uppercase; height: 38px !important;" name="medico">
                                 <?php while ($dadoMedic = $medic->retornaDados("object")) { ?>  
               									 <option value="<?php echo $dadoMedic->IDMEDICO; ?>"><?php echo $dadoMedic->NOME; ?></option>
                                 <?php  }  ?>
@@ -40,7 +40,7 @@
                           <form action="../Relatorio/Agenda/RelatorioAgendaPaciente.php" method="POST" target="_blank">
                           <div class="modal-body">
                             <div class="conteudo">              
-                              <select class="custom-select" style="text-transform: uppercase;" name="paciente">
+                              <select class="custom-select" style="text-transform: uppercase; height: 38px !important;" name="paciente">
                                 <?php while ($dadoPac = $pac->retornaDados("object")) { ?>  
                                 <option value="<?php echo $dadoPac->IDPACIENTE; ?>"><?php echo $dadoPac->NOME; ?></option>
                                 <?php  }  ?>
@@ -69,7 +69,7 @@
                           <form action="../Relatorio/Agenda/RelatorioAgendaAtendimento.php" method="POST" target="_blank">
                           <div class="modal-body">
                             <div class="conteudo">              
-                              <select class="custom-select" style="text-transform: uppercase;" name="atendimento">
+                              <select class="custom-select" style="text-transform: uppercase; height: 38px !important;" name="atendimento">
                                 <?php while ($dadoAtend = $tipoAten->retornaDados("object")) { ?>  
                                 <option value="<?php echo $dadoAtend->IDATENDIMENTO; ?>"><?php echo $dadoAtend->TIPOATENDIMENTO; ?></option>
                                 <?php  }  ?>
@@ -98,7 +98,7 @@
                           <form action="../Relatorio/Paciente/RelatorioPacienteSexo.php" method="POST" target="_blank">
                           <div class="modal-body">
                             <div class="conteudo">              
-                              <select class="custom-select" style="text-transform: uppercase;" name="sexo">
+                              <select class="custom-select" style="text-transform: uppercase; height: 38px !important;" name="sexo">
                                 <option value="Masculino">MASCULINO</option>
                                 <option value="Feminino">FEMININO</option>
                               </select>
@@ -129,11 +129,11 @@
                             <div class="row">             
                               <div class="form-group col-sm-6">
                                 <label for="diaIni">Data Inicial:</label>
-                                <input class="form-control" type="date" name="DataInicial" id="diaIni" required>
+                                <input class="form-control" type="date" name="DataInicial" id="diaIni" style="height: 38px !important;" required>
                               </div>
                               <div class="form-group col-sm-6">
                                 <label for="diaFim">Data Final:</label>
-                                <input class="form-control" type="date" name="DataFinal" id="diaFim" required>
+                                <input class="form-control" type="date" name="DataFinal" id="diaFim" style="height: 38px !important;" required>
                               </div>
                             </div>
                             </div>
@@ -163,7 +163,7 @@
                             <div class="row">             
                               <div class="form-group col-sm-6" style="margin: 0 auto; ">
                                 <label for="date">DATA DE CADASTRO:</label>
-                                <input class="form-control" type="date" name="data_registro_paciente" id="date" value="<?php echo date("Y-m-d"); ?>" required>
+                                <input class="form-control" type="date" name="data_registro_paciente" id="date" style="height: 38px !important;" value="<?php echo date("Y-m-d"); ?>" required>
                               </div>
                             </div>
                             </div>
@@ -191,7 +191,7 @@
                           <div class="modal-body">
                             <div class="conteudo">
                             <label for="mes_registro_paciente">MÊS DE CADASTRO:</label>
-                            <select class="custom-select" style="text-transform: uppercase;" name="mes_registro_paciente">
+                            <select class="custom-select" style="text-transform: uppercase; height: 38px !important;" name="mes_registro_paciente">
                                 <option value="Janeiro">JANEIRO</option>
                                 <option value="Fevereiro">FEVEREIRO</option>
                                 <option value="Marco">MARÇO</option>
@@ -230,7 +230,7 @@
                           <div class="modal-body">
                             <div class="conteudo">
                             <label for="mes_registro_paciente">MÊS DE CADASTRO:</label>
-                            <select class="custom-select" style="text-transform: uppercase;" name="mes_registro_paciente">
+                            <select class="custom-select" style="text-transform: uppercase; height: 38px !important;" name="mes_registro_paciente">
                                 <option value="Janeiro">JANEIRO</option>
                                 <option value="Fevereiro">FEVEREIRO</option>
                                 <option value="Marco">MARÇO</option>
@@ -269,7 +269,7 @@
                           <div class="modal-body">
                             <div class="form-group col-sm-6" style="margin: 0 auto; ">
                                 <label for="date">ANO DE CADASTRO:</label>
-                                <input type="text" name="anoRegistroPaciente" id="anoCadastro" class="form-control" required>
+                                <input type="text" name="anoRegistroPaciente" id="anoCadastro" class="form-control" style="height: 38px !important;" required>
                             </div>
                           </div>
                           <div class="modal-footer">
@@ -295,7 +295,7 @@
                           <div class="modal-body">
                             <div class="conteudo">
                             <label for="mes_registro_paciente">MÊS DE NASCIMENTO:</label>
-                            <select class="custom-select" style="text-transform: uppercase;" name="dataMesNiver">
+                            <select class="custom-select" style="text-transform: uppercase; height: 38px !important;" name="dataMesNiver">
                                 <option value="Janeiro">JANEIRO</option>
                                 <option value="Fevereiro">FEVEREIRO</option>
                                 <option value="Marco">MARÇO</option>
@@ -332,9 +332,9 @@
                           </div>
                           <form action="../Relatorio/Paciente/RelatorioPacienteAnoNiver.php" method="POST" target="_blank">
                           <div class="modal-body">
-                            <div class="form-group col-sm-6" style="margin: 0 auto; ">
+                            <div class="form-group col-sm-6" style="margin: 0 auto;">
                                 <label for="date">ANO DE NASCIMENTO:</label>
-                                <input type="text" name="dataAnoNiver" id="anoCadastro" class="form-control" required>
+                                <input type="text" name="dataAnoNiver" id="anoCadastro" class="form-control" style="height: 38px !important;" required>
                             </div>
                           </div>
                           <div class="modal-footer">
