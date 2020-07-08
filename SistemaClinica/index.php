716772f1997eb2649968eb81a8df9ee85068129f
J-Eugenio <eugeniopb4@gmail.com>
2020-07-08 13:37:28 -0300

    Ajustes

--- SistemaClinica/index.php
+++ SistemaClinica/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<meta http-equiv="refresh" content="0;url=/SistemaClinica/Telas/Index.php">
+<meta http-equiv="refresh" content="0;url=/Clinica/SistemaClinica/Telas/Index.php">
 </head>
 <body>
 <?php
